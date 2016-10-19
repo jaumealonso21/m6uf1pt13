7fc8372825abb99731c0167fcbe405cb7705eea8
jaumealonso21 <jaumealonso@hotmail.com>
2016-10-19 16:46:24 +0200

    Versio 2.0 final

--- public_html/m6uf1pt13.js
+++ public_html/m6uf1pt13.js
@@ -1,3 +1,4 @@
+
 var finestra;
 
 function crear() {
@@ -16,6 +17,37 @@ function mover() {
 
 function moverFix() {
     window.finestra.moveTo(arguments[0], arguments[1]);
+    window.finestra.focus();
+    
+    return true;
+}
+
+function canviaTamany() {
+    window.finestra.resizeBy(arguments[0], arguments[1]);
+    window.finestra.focus();
+    
+    return true;
+}
+
+function canviaFixTamany() {
+    window.finestra.resizeTo(arguments[0], arguments[1]);
+    window.finestra.focus();  
+    
+    return true;
+}
+
+function missatge() {
+    window.finestra.document.write("<script>function missPare() { window.opener.document.getElementById(\'desti\').innerHTML = arguments[0]; }</script>");
+    window.finestra.document.write("<div>" + arguments[0] + "</div>");
+    window.finestra.document.write("<div><input type='text' id='miss2' value='Escriu el missatge...' />");
+    window.finestra.document.write("<input type=\"button\" onclick=\"missPare(document.getElementById(\'miss2\').value);\" value=\"Envia missatge\" /><br /></div>");
+    window.finestra.focus();
+    
+    return true;
+}
+
+function imatge() {
+    window.finestra.document.write("<div><img src='imatge.jpg' /></div>");
     window.finestra.focus();
     
     return true;
